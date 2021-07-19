cf963c281dc87eed245d113019e2003d111c5631
littlehobbit <rusikarm3@gmail.com>
2021-07-19 18:36:14 +0300

    understanding new github auto policy

--- word-counter/main.cpp
+++ word-counter/main.cpp
@@ -6,14 +6,5 @@
 
 int main(int argc, char **argv) {
 
-    btl::list<int> ilist;
-    ilist.push_back(10);
-    ilist.push_back(15);
-
-    auto even = std::find_if(ilist.begin(), ilist.end(), [](int el) -> bool { return el % 2 == 0; });
-    if (even != ilist.end()) {
-        std::cout << "Was found " << *even << std::endl;
-    }
-
     return 0;
 }
